f0102d510e40fdbeb3474147e803f2e0e3744376
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-02-18 20:33:30 +0330

    Added the dynamic active menu

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,19 +2,30 @@ import type { NextComponentType } from "next";
 import Link from "next/link";
 import classes from "~/styles/components/Navbar/index.module.scss";
 import { FaSearch, FaAddressCard, FaHome } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 const Navbar: NextComponentType = () => {
+  const router = useRouter();
   return (
     <nav className={classes.container}>
       <div className={classes.left}>
         <Link href="/" passHref>
-          <span className={`${classes.navbar_item} active`}>
+          <span
+            className={`${classes.navbar_item} ${
+              router.asPath == "/" ? "active" : null
+            }`}
+          >
             <FaHome />
             Home
           </span>
         </Link>
         <Link href="/aboutme" passHref>
-          <span className={classes.navbar_item}>
+          <span
+            className={`${classes.navbar_item} ${
+              router.asPath == "/aboutme" ? "active" : null
+            }`}
+          >
+            {" "}
             <FaAddressCard />
             About me
           </span>
